8988827e9ed11c7fb86afe2f018e0e3464756f2c
Lixad <filip.wesolowski@onet.eu>
2022-11-14 00:11:58 +0100

    Add css reset, create file structure, add readme (#2)
    Add css reset, create file structure, add readme

---
+++ js/shared_footer.js
@@ -0,0 +1,11 @@
+const footer = document.getElementById('shared-footer');
+footer.innerHTML = `
+  <header>
+    <div>Contact</div>
+    <a href="#">About</a>
+    <a href="#">Career</a>
+    <a href="#">Our offer</a>
+    <a href="#">Contact</a>
+    <a href="#">Chuj wie</a>
+  </header>
+`;
---
+++ js/shared_header.js
@@ -0,0 +1,11 @@
+const header = document.getElementById('shared-header');
+header.innerHTML = `
+  <header>
+    <a href="#">Logo</a>
+    <a href="#">About</a>
+    <a href="#">Career</a>
+    <a href="#">Our offer</a>
+    <a href="#">Contact</a>
+    <a href="#">Chuj wie</a>
+  </header>
+`;
